2418a7239a5066fecfb71cf5c2360ae67ae56849
harkamal singh <harkamal.singh@nagarro.com>
2014-11-18 22:32:42 +0530

    implemented user sign in functionality. Also added the login and sign up links on header. defined the root path of the application. Added the test cases.

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature 'Home Page' do
+  scenario "user visits the home page" do
+    visit('/')
+    expect(page).to have_content('Welcome to My Gadgets')
+  end
+end
+
+
